d7ae1349c9fa868de1dd1de4180117d37e010070
Vatsal Radadiya <radadiyavatsal@gmail.com>
2024-06-04 00:07:13 +0530

    Try 3

--- models/User.js
+++ models/User.js
@@ -1,5 +1,3 @@
-const { toBeRequired } = require('@testing-library/jest-dom/matchers');
-const { type } = require('@testing-library/user-event/dist/type');
 const mongoose=require('mongoose');
 const {Schema} = mongoose;
 const UserSchema = new Schema({
