8040825edb59529e831426b75acbd929d651fc9c
Vatsal Radadiya <radadiyavatsal@gmail.com>
2024-06-04 12:17:10 +0530

    Deploying

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function startServer() {
 
   // CORS configuration
   const corsOptions = {
-    origin: 'https://your-render-frontend-domain.onrender.com', // Update with your frontend domain on Render.com
+    origin: 'https://inotebook-qlhs.onrender.com', // Update with your frontend domain on Render.com
     methods: 'GET,POST,PUT,DELETE', // Allowed methods
     allowedHeaders: 'Content-Type,Authorization,auth-token', // Allowed headers
   };
